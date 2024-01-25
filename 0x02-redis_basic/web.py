663dc2b28438b9cd14b43be6ef9fc579cdd8f370
globalsmile <mohammedbadmus823@gmail.com>
2024-01-25 03:13:25 +0000

    reversion: web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,27 +1,22 @@
 #!/usr/bin/env python3
 """ Implementing an expiring web cache and tracker
     obtain the HTML content of a particular URL and returns it """
-import requests
-import time
 import redis
+import requests
 r = redis.Redis()
+count = 0
 
 
 def get_page(url: str) -> str:
     """ track how many times a particular URL was accessed in the key
         "count:{url}"
         and cache the result with an expiration time of 10 seconds """
-    if r.get(f'count:{url}'):
-        r.incr(f'count:{url}')
-        return r.get(f'count:{url}')
-    else:
-        r.set(f'count:{url}', 1)
-        r.expire(f'count:{url}', 10)
-        return requests.get(url).text
+    r.set(f"cached:{url}", count)
+    resp = requests.get(url)
+    r.incr(f"count:{url}")
+    r.setex(f"cached:{url}", 10, r.get(f"cached:{url}"))
+    return resp.text
+
 
 if __name__ == "__main__":
-    url = 'http://slowwly.robertomurray.co.uk'
-    get_page(url)
-
-    
-
+    get_page('http://slowwly.robertomurray.co.uk')
